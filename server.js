2eaa6e223b1f4f621b833ce987c906c2850b9cee
Rain Holloway <rainh2001@gmail.com>
2019-07-01 16:26:31 +1000

    Implemented NodeJS and ExpressJS to run the website off a server to allow XHR transfer of JSON data

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const app = express();
+const port = 8080;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.listen(port, function(){
+    console.log(`Server running on port: ${port}`);
+});
+
+app.get("/highscore.json", function(req,res) {
+    res.send(fs.readFileSync("highscore.json"));
+});
+
+app.post("/highscore.json", function(req, res){
+    console.log(req.body);                                                                             
+    let highscore = JSON.parse(fs.readFileSync("highscore.json"));
+    let newHighscore = false;
+    if(highscore.length < 10){
+        newHighscore = true;
+    } else if(req.body.score > highscore[highscore.length-1].score){
+        highscore.pop();
+        newHighscore = true;
+    }                                                               
+    if(newHighscore){
+        highscore.push({
+            username: req.body.username,
+            score: req.body.score
+        });
+    }
+    if(highscore.length > 1){
+        let i = highscore.length-1;
+        let temp = highscore[i];
+        while(i > 0 && temp.score > highscore[i-1].score){
+            highscore[i] = highscore[i-1];
+            i--;
+        }
+        highscore[i] = temp;
+    }
+    highscore = JSON.stringify(highscore, null, 4);
+    fs.writeFileSync("highscore.json", highscore);
+    res.send(highscore);
+});
